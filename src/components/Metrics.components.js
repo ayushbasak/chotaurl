722ee5cc4e57b8a579243c9eaef045359a518c52
ayushbasak <ayushbasak0210@gmail.com>
2021-06-08 00:42:48 +0530

    [UI] Change Metric Component UI

--- src/components/Metrics.components.js
+++ src/components/Metrics.components.js
@@ -21,11 +21,14 @@ const Metrics = ()=>{
             <div className="flex flex-col lg:flex-row h-4/6 justify-evenly items-center">
                 <p className="text-5xl bg-green-400 p-3 rounded-xl text-white shadow-xl text-center">{count} <br></br>entries</p>
                 <div className="overflow-auto h-4/6 w-full lg:w-4/12 p-2 bg-blue-800 text-center lg:rounded-xl shadow-xl">
-                <span className="text-white text-2xl bg-red-400 p-4 rounded-xl">Domains</span>
+                <span className="text-white text-2xl bg-red-400 p-4 rounded-xl flex flex-col justify-center items-center">{hostNames.length} domains</span>
                         {
                             
                             hostNames.map(curr =>
-                                   <p className="text-white text-center select-none">{curr}</p>    
+                                <span className="flex flex-row flex flex-row justify-left items-center p-1">
+                                    <img src={`https://${curr}/favicon.ico`} className="w-5 h-5 m-2 mx-5 bg-white-200" alt="N"/>
+                                    <span className="text-white text-center select-none">{curr}</span>    
+                                </span>
                             )
                         }
                 </div>
