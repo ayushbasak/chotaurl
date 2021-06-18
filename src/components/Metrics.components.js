a9538f56e62460eb8be8349f11b13550deef9a57
ayushbasak <ayushbasak0210@gmail.com>
2021-06-18 18:06:54 +0530

    [Feature] Add Authentication
    - Auth0 Integration with React
    - Update User Component with Admin

--- src/components/Metrics.components.js
+++ src/components/Metrics.components.js
@@ -19,7 +19,7 @@ const Metrics = ()=>{
         {
            count >= 0 ? 
             <div className="flex flex-col lg:flex-row h-4/6 justify-evenly items-center">
-                <p className="text-5xl bg-green-400 p-3 rounded-xl text-white shadow-xl text-center">{count} <br></br>entries</p>
+                <p className="text-5xl bg-green-400 p-5 w-11/12 lg:w-5/12 rounded-xl text-white shadow-xl text-center">{count} entr{count === 1 ? 'y' : 'ies'}</p>
                 <div className="overflow-auto h-4/6 w-full lg:w-4/12 p-2 bg-blue-800 text-center lg:rounded-xl shadow-xl">
                 <span className="text-white text-2xl bg-red-400 p-4 rounded-xl flex flex-col justify-center items-center">{hostNames.length} domains</span>
                         {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 ReactDOM.render(
-  <App />,
+  <Auth0Provider
+    domain = 'ayushbasak.us.auth0.com'
+    clientId = 'qfC7IaLf0kD2CWyKhNwkJbj4yl1iHmYF'
+    redirectUri={window.location.origin}
+    >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
